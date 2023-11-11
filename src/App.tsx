f7bc4ce6f897b70458ea515201c87ae8d13ce5ab
minthukha1288 <minthukha1239788@gmail.com>
2023-11-11 12:29:06 +0630

    tsx bay din

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import "./App.css";
-import Question from "./components/Question";
+import Questions from "./components/Question";
 
 function App() {
   return (
     <>
       <div>
-        <h1 className="bg-red-700">Lat Htauk BayDin (Min Thein Kha)</h1>
-        <Question />
-        <p>by lemon</p>
+        <h1 className="text-xl">Lat Htauk BayDin (Min Thein Kha)</h1>
+        <Questions />
+        <p>© 2023 BayDin by Lemon</p>
       </div>
     </>
   );
